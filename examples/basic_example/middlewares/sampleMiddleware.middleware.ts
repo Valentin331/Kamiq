f6bea0b0b902163650cc873820c63f51aad392c1
Valentin Kuharic <valentinkuharic12@gmail.com>
2023-08-09 12:50:55 +0200

    fixed publishing workflow

--- examples/basic_example/middlewares/sampleMiddleware.middleware.ts
+++ examples/basic_example/middlewares/sampleMiddleware.middleware.ts
@@ -1,24 +1,22 @@
-import { Request, Response, NextFunction } from "express";
-import { KamiqMiddleware } from "kamiq/interfaces";
+import { Request, Response, NextFunction } from 'express'
+import { KamiqMiddleware } from 'kamiq/interfaces'
 
 const log = console.log
 
 export class MySampleMiddleware implements KamiqMiddleware {
-    private readonly someValue: boolean;
+  private readonly someValue: boolean
 
-    constructor(someValue: boolean) {
-        this.someValue = someValue;
-      }
+  constructor(someValue: boolean) {
+    this.someValue = someValue
+  }
 
-    async use(req: Request, res: Response, next: NextFunction): Promise<void> {
-        
-        // Can use someValue to change behavior...
+  async use(req: Request, res: Response, next: NextFunction): Promise<void> {
+    // Can use someValue to change behavior...
 
-        log('sample middleware hit!')
-        // Modifying the request object:
-        // @ts-ignore
-        req.something = 'this is nice'
-        next()
-    }
-
+    log('sample middleware hit!')
+    // Modifying the request object:
+    // @ts-ignore
+    req.something = 'this is nice'
+    next()
+  }
 }
